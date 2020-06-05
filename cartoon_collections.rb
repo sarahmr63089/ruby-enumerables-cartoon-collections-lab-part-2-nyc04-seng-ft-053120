ecc5fa5766af39907220f44739cba96e7670d5dc
Sarah Rose <sarahrose63089@gmail.com>
2020-06-05 19:30:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,3 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
 end
-
-planeteer_calls = ["hello", "sarah"]
-
-summon_captain_planet(planeteer_calls)
